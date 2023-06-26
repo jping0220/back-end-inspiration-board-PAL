3c478b0635b267aca9fd6bbaac55c58d271df310
Limary Gonzalez <limaryg11@gmail.com>
2023-06-26 14:06:08 -0700

    added board post and get routes

--- app/models/board.py
+++ app/models/board.py
@@ -4,3 +4,19 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.board_id,
+            "title": self.title,
+            "owner": self.owner,
+        }
+    
+    @classmethod
+    def from_dict(cls, board_data):
+        new_board = cls(
+            title = board_data["title"],
+            owner = board_data["owner"],
+        )
+        
+        return new_board
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,51 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
+from app.models.board import Board
+from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
+board_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+def validate_item(model, item_id):
+    try:
+        item_id = int(item_id)
+    except ValueError:
+        return abort(make_response({"details": "Invalid data"}, 400))
+    
+    item = model.query.get(item_id)
+
+    if not item:
+        return abort(make_response({"details": f"id {item_id} not found"}, 404))
+    
+    return item
+
+@board_bp.route("", methods=["POST"])
+def add_boards():
+    request_body = request.get_json()
+
+    new_board = Board.from_dict(request_body)
+
+    db.session.add(new_board)
+    db.session.commit()
+
+    return {"board": new_board.to_dict()}, 201
+
+@board_bp.route("", methods=["GET"])
+def get_all_boards():
+    response = []
+
+    all_boards = Board.query.all()
+
+    for board in all_boards:
+        response.append(board.to_dict())
+    
+    return jsonify(response), 200
+
+@board_bp.route("/<board_id>", methods=["GET"])
+def get_one_board(board_id):
+    board = validate_item(Board, board_id)
+    return {"board": board.to_dict()}, 200
+
+
+
+    
